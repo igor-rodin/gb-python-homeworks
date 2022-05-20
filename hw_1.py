b708fc608dd8020aca082eff97224e8387059c7b
Igor Rodin <igreen1974@gmail.com>
2022-05-20 19:13:28 +0300

    Done task 2

--- hw_1.py
+++ hw_1.py
@@ -2,9 +2,29 @@
 # Сформировать список из  N членов последовательности.
 # Для N = 5: 1, -3, 9, -27, 81 и т.д. (-3)**(N-1)
 
-def gen_powers_of_three(num):
+def powers_of_three(num):
     return [(-3)**num for num in range(num)]
 
 
 n = 5
-print(gen_powers_of_three(n))
+print(powers_of_three(n))
+
+# Задача 2
+# Пользователь задаёт две строки. Определить количество вхождений одной строки в другой.
+
+
+def count_str(substr, string):
+    idx = 0
+    count = 0
+    idx = string.find(substr, idx)
+    while idx != -1:
+        count += 1
+        idx = string.find(substr, idx + len(substr))
+    return count
+
+
+string = "PythonIsPyt where Pyth wherePyth"
+what = "where"
+
+print(
+    f"Количество вхождений '{what}' in '{string}' - {count_str(what, string)}")

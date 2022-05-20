1b240691e089f365056ea4be6ad7ffa39c2786ee
Igor Rodin <igreen1974@gmail.com>
2022-05-20 18:55:13 +0300

    Done task 1

--- hw_1.py
+++ hw_1.py
@@ -1,3 +1,10 @@
 # Задача 1
 # Сформировать список из  N членов последовательности.
 # Для N = 5: 1, -3, 9, -27, 81 и т.д. (-3)**(N-1)
+
+def gen_powers_of_three(num):
+    return [(-3)**num for num in range(num)]
+
+
+n = 5
+print(gen_powers_of_three(n))

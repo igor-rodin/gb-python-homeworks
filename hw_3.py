ce16b1d250165f9115a8706c0891178e7e96c033
Igor Rodin <igreen1974@gmail.com>
2022-05-27 19:40:39 +0300

    Done task 2

--- hw_3.py
+++ hw_3.py
@@ -1,6 +1,9 @@
 # 1. Найти НОК двух чисел
 # lcm(a, b) = (a * b)/gcd(a, b)
 
+import math
+
+
 def gcd(a, b):
     if a < b:
         return gcd(b, a)
@@ -19,3 +22,39 @@ a = 16
 b = 20
 
 print(f"НОК({a},{b}) = {lcm(a, b)}")
+
+# 2 Вычислить число pi c заданной точностью d
+# Пример: при d = 0.001,  c= 3.141.
+# Для вычисления используется ряд Лейбница pi/4 = Sum((-1)**n/(2 * n + 1), n = 0..inf)
+
+
+def calc_pi(epsilon):
+    n = 2
+    pi = 4*[1, -1/3]
+    dif = 1
+    while dif > epsilon:
+        pi.append(4*(-1)**n/(2 * n + 1))
+        dif = abs(sum(pi[:-2]) - sum(pi[:-1]))
+        n += 1
+
+    str_eps = str(epsilon)
+    round_val = len(str_eps[str_eps.find('.')+1:])
+    return round(sum(pi), round_val)
+
+
+d = 0.001
+print("Первый вариант")
+print(f"Число pi(с точностью {d}) = {calc_pi(d)}")
+
+# Вариант просто округления числа с нужной точность
+
+
+def calc(number, epsilon):
+    str_eps = str(epsilon)
+    round_val = len(str_eps[str_eps.find('.')+1:])
+    return round(number, round_val)
+
+
+print("Второй вариант")
+number = 3.141093153121447
+print(f"Число {number}(с точностью {d}) = {calc(number, d)}")

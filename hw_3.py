db2e0b475cd8183b064fff443b847f4046eb0a85
Igor Rodin <igreen1974@gmail.com>
2022-05-29 12:54:42 +0300

    Done extra task 3

--- hw_3.py
+++ hw_3.py
@@ -33,31 +33,16 @@ def calc_pi(epsilon):
     dif = 1
     while dif > epsilon:
         pi.append(4*(-1)**n/(2 * n + 1))
-        dif = abs(sum(pi[:-2]) - sum(pi[:-1]))
+        dif = abs(sum(pi[:-1]) - sum(pi[:-2]))
         n += 1
 
-    str_eps = str(epsilon)
-    round_val = len(str_eps[str_eps.find('.')+1:])
-    return round(sum(pi), round_val)
+    return math.floor(sum(pi)*int(1/epsilon))*epsilon
 
 
 d = 0.001
 print("-------------------------\nЗадача 2")
-print("Первый вариант")
 print(f"Число pi(с точностью {d}) = {calc_pi(d)}")
 
-# Вариант просто округления числа с нужной точность
-
-
-def calc(number, epsilon):
-    str_eps = str(epsilon)
-    round_val = len(str_eps[str_eps.find('.')+1:])
-    return round(number, round_val)
-
-
-print("Второй вариант")
-number = 3.141093153121447
-print(f"Число {number}(с точностью {d}) = {calc(number, d)}")
 
 # 3 Составить список простых множителей натурального числа N
 
@@ -104,11 +89,6 @@ def get_uniq(arr):
 
     return list_uniq
 
-
-print("-------------------------\nЗадача 4")
-arr = [1, 2, 3, 5, 1, 5, 3, 10]
-print(f"Неповторояющиеся элементы списка {arr} -> {get_uniq(arr)}")
-
 # Через приведение к множеству
 
 
@@ -116,7 +96,9 @@ def get_uniq_2(arr):
     return list(set(arr))
 
 
-print(f"Неповторояющиеся элементы списка {arr} -> {get_uniq_2(arr)}")
+print("-------------------------\nЗадача 4")
+arr = [1, 2, 3, 5, 1, 5, 3, 10]
+print(f"Неповторояющиеся элементы списка {arr} -> {get_uniq(arr)}")
 
 # Экстра-задачи:
 
@@ -128,6 +110,8 @@ print(f"Неповторояющиеся элементы списка {arr} -> 
 # Найдите разность между суммой квадратов и квадратом суммы первых ста натуральных чисел.
 # S_n = n*(n+1)/2- сумма первых n натуральных чисел, S_n2 = n*(n+1)*(2 * n +1)/6 - сумма квадратов первых n натуральных чисел
 
+# По формуле
+
 
 def std(n):
     sum_n = n*(n+1)//2
@@ -135,8 +119,81 @@ def std(n):
 
     return sum_n**2 - sum_n2
 
+# Циклом
+
+
+def std_2(n):
+    sum_of_squares = sum([i**2 for i in range(1, n+1)])
+    square_of_sum = sum([i for i in range(1, n+1)])**2
+
+    return square_of_sum - sum_of_squares
+
 
 print("-------------------------\nExtra задача 4")
 N = 100
 print(
     f"Разность между квадратом суммы и суммой квадратов первых {N} натуральных чисел = {std(N)}")
+
+# 3 Начиная в вершине треугольника (см. пример ниже) и перемещаясь вниз на смежные числа, максимальная сумма до основания составляет 23.
+# 3
+# 7 4
+# 2 4 6
+# 8 5 9 3
+# То есть, 3 + 7 + 4 + 9 = 23.
+# Найдите максимальную сумму пути от вершины до основания следующего треугольника:
+# 75
+# 95 64
+# 17 47 82
+# 18 35 87 10
+# 20 04 82 47 65
+# 19 01 23 75 03 34
+# 88 02 77 73 07 63 67
+# 99 65 04 28 06 16 70 92
+# 41 41 26 56 83 40 80 70 33
+# 41 48 72 33 47 32 37 16 94 29
+# 53 71 44 65 25 43 91 52 97 51 14
+# 70 11 33 28 77 73 17 78 39 68 17 57
+# 91 71 52 38 17 14 91 43 58 50 27 29 48
+# 63 66 04 68 89 53 67 30 73 16 69 87 40 31
+# 04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+
+# Элементы треугольника хранятся в файле triangle_path. Строки файла соответсвуют строкам треугольника
+triangle_file = 'triangle.txt'
+
+
+def read_triangle(file_triangle: str) -> list:
+    '''
+    Метод считывает из файла file_triangle данные треугольника и возвращает
+    массив с элементами треугольника
+    '''
+    with open(file_triangle, 'r') as tri:
+        lines = tri.readlines()
+        tri_arr = [[0 for _ in range(len(lines))] for _ in range(len(lines))]
+        i = 0
+        for line in lines:
+            tr_raw = line.split()
+            for j in range(len(tr_raw)):
+                tri_arr[i][j] = int(tr_raw[j])
+            i += 1
+    return tri_arr
+
+
+def max_path(list):
+    summa = list[0][0]
+    max_j = 0
+    for r in range(len(list[0])-1):
+        left = list[r + 1][max_j]
+        right = list[r + 1][max_j + 1]
+        max_val = max(left, right)
+        max_j = max_j if max_val == left else max_j + 1
+        summa += max_val
+    return summa
+
+
+def max_triangle_path(file_triangle):
+    triangle_list = read_triangle(file_triangle)
+    return max_path(triangle_list)
+
+
+print("-------------------------\nExtra задача 3")
+print(f"Максимальна сумма пути: {max_triangle_path(triangle_file)}")
